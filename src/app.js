bcdfc717ae7338838ec92a6b7e30d3272194d039
Elirrez8 <elirrez8@gmail.com>
2022-08-03 14:16:27 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,59 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+/*
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNum();
+};
+
+let generateRandomNum = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "9", "10", "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamonds", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+*/
+/*♦ ♥ ♠ ♣*/
+window.onload = function() {
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let numero = numeros[Math.floor(Math.random() * (13 - 0) + 0)];
+
+  var palos = [
+    { s: "♦", color: "red" },
+    { s: "♥", color: "red" },
+    { s: "♠", color: "black" },
+    { s: "♣", color: "black" }
+  ];
+
+  let palo = palos[Math.floor(Math.random() * (4 - 0) + 0)];
+
+  document.querySelectorAll(".palo").forEach(elem => {
+    elem.innerHTML = palo.s;
+    elem.style.color = palo.color;
+  });
+
+  document.querySelector(".numero").innerHTML = numero;
+  document.querySelector(".numero").style.color = palo.color;
 };
